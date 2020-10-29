97e4ec73c514953ee3d43cb7f12be0ed1e86db7c
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-29 13:28:17 +0100

    Basic HTML & CSS structure completed

---
+++ action.js
@@ -0,0 +1,13 @@
+/* ASSIGNMENT
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi, vengono rimossi i numeri dalla pagina
+e l'utente deve inserire (tramite prompt) i numeri che ha visto precedentemente, uno alla volta.
+Dopo che sono stati inseriti i 5 numeri,
+il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+BONUS: visualizzare in pagina anche un timer con il countdown dei 30 secondi
+*/
+
+$(document).ready(function() {
+
+});
