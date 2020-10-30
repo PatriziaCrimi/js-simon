f657d954d4f590d3200cef9120d7fd90f8d3afc0
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-30 13:11:44 +0100

    Exercise completed with bonus (bug in Countdown  timing to be fixed)

--- action.js
+++ action.js
@@ -9,17 +9,17 @@ e quali dei numeri da indovinare sono stati individuati.
 BONUS: visualizzare in pagina anche un timer con il countdown dei 30 secondi
 */
 
-// Varaibles and constants initialization
+// Variables and constants initialization
 const min_number = 1;
 const max_number = 100;
 const numbers_quantity = 5;
 var random_numbers_array = [];
-const remove_timer = 30000;
-const input_timer = 31000;
+const seconds_countdown = 30;
+var remove_timer = seconds_countdown * 1000;
+var input_timer = remove_timer + 1000;
 var user_numbers_array = [];
 var guessed_numbers_array = [];
 var countdown_stop;
-var i = 0;
 
 $(document).ready(function() {
   // Generating random numbers
@@ -30,21 +30,24 @@ $(document).ready(function() {
     if (!random_numbers_array.includes(random_number)) {
       // Storing the new number in the array (if it is NOT a duplicate)
       random_numbers_array.push(random_number);
-      i++
     }
-  } while (i < numbers_quantity);
+  } while (random_numbers_array.length < numbers_quantity);
   console.log('The array containing the random numbers is: ' , random_numbers_array);
 
   // ------------------------- DOM MANIPULATION -------------------------
 
-  // Printing random numbers in the web page (HTML)
-  for ( i = 0; i < random_numbers_array.length; i++) {
+  // Showing random numbers in the web page (HTML)
+  $('#random-numbers').append('<h2>Random Numbers</h2>');
+  $('#random-numbers').append('<p>You have ' + seconds_countdown + ' seconds to memorize the following numbers:</p>');
+  for (var i = 0; i < random_numbers_array.length; i++) {
     $('#random-numbers').append('<span>' + random_numbers_array[i] + '</span>');
   }
-  // Starting the countdown to remove numbers from HTML
+  // Starting the countdown (to remove numbers from HTML)
   setTimeout(removeNumbers, remove_timer);
   // Showing the countdown on the screen
   countdown_stop = setInterval(countdown, 1000);
+  $('#countdown-section').prepend('<p>Remaining time:</p>');
+  $('#countdown-section').prepend('<h2>Countdown</h2>');
   // Entering inputs from the user after the previous countdown has ended
   setTimeout(guessingNumbers, input_timer);
 });
@@ -57,26 +60,35 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
 
-// Removing numbers from HTML & stopping countdown
-function removeNumbers() {
-  $('#random-numbers > span').remove();
-  clearInterval(countdown_stop);
-};
-
 // Countdown of numbers on the screen
 var k = 0;
 function countdown() {
-
-  var seconds = k + 1;
-  while (k < seconds) {
-    $('#countdown-section').append('<span>' + seconds + '</span>');
-    k++
-  }
-  // $('#countdown-section > span').remove();
+  var seconds_visualized = seconds_countdown - k;
+  $('#countdown-section > span').text(seconds_visualized);
+  k++;
 }
 
-// User's guesses about the numbers he/she can recollect and results
+// Stopping countdown on the screen
+function stopCountdown() {
+  clearInterval(countdown_stop);
+  $('#countdown-section > h2').remove();
+  $('#countdown-section > p').remove();
+  $('#countdown-section > span').remove();
+}
+
+// Removing numbers while stopping countdown on the screen
+function removeNumbers() {
+  // Stopping countdown on the screen
+  stopCountdown();
+  // Removing numbers
+  $('#random-numbers > h2').remove();
+  $('#random-numbers > p').remove();
+  $('#random-numbers > span').remove();
+};
+
+// User's guesses about the numbers he/she can recollect & results/output
 function guessingNumbers() {
+
   // ***** Entering inputs from the user (the numbers he/she can remember) *****
   do {
     // Entering inputs
@@ -107,23 +119,39 @@ function guessingNumbers() {
     }
   }
 
-  // ***** Messages to the user & console *****
+  // ***** Messages to the user & console & output in the DOM *****
+
+  // Ouput in the DOM - "Results" section header
+  $('#results-section').append('<h2>Results</h2>');
+  // Results
   if (guessed_numbers_array.length) {
     if (guessed_numbers_array.length === numbers_quantity) {
     // Message about recollecting ALL of the numbers
     console.log('The user could recollect all of the ' + numbers_quantity + '  numbers!');
     alert('CONGRATULATIONS! You could recollect all of the ' + numbers_quantity + ' numbers!');
+    // Ouput in the DOM
+    $('#results-section').append('<h3>CONGRATULATIONS!</h3>');
+    $('#results-section').append('<p>You could recollect all of the ' + numbers_quantity + ' numbers!</p>');
     } else {
     // Message about HOW MANY numbers he/she could recollect
     console.log('The user recollected ' + guessed_numbers_array.length + ' numbers.');
     alert('You recollected ' + guessed_numbers_array.length + ' numbers.');
+    // Ouput in the DOM
+    $('#results-section').append('<p>You recollected ' + numbers_quantity + ' numbers.</p>');
     }
     // Message about WHICH numbers he/she recollected
-    console.log('The array containing all the guessed numbers is: ' , guessed_numbers_array);
-    alert('Number(s) recollected: ' + guessed_numbers_array);
+    console.log('The array containing all of the guessed numbers is: ' , guessed_numbers_array);
+    alert('Number(s) recollected: ' + guessed_numbers_array.join(' - ') + '.');// Ouput in the DOM
+    $('#results-section').append('<h3>Number(s) recollected:</h3>');
+    $('#results-section').append('<span>' + guessed_numbers_array.join(' - ') + '</span>');
   } else {
     // Message about NOT recollecting any number
     console.log('The user couldn\'t recollect any number.');
     alert('You couldn\'t recollect any number. We strongly recommend you go to the doctor and check your memory!');
+    // Ouput in the DOM
+    $('#results-section').append('<p>You couldn\'t recollect any number.</p>');
+    $('#results-section').append('<p>We strongly recommend you go to the doctor and check your memory!</p>');
   }
+  $('#results-section').append('<h3>The shown numbers were:</h3>');
+  $('#results-section').append('<span>' + random_numbers_array.join(' - ') + '</span>');
 }
